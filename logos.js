a6faa6d335ca3b67f5055b3586120c74045ec11f
Joseph Ogbu <57672199+fallyfox@users.noreply.github.com>
2022-08-31 16:04:52 +0100

    Delete logos.js

--- logos.js
+++
@@ -1,6 +0,0 @@
-// https://cdn-icons-png.flaticon.com/512/2869/2869818.png
-// https://cdn-icons-png.flaticon.com/512/1048/1048611.png
-// https://cdn-icons-png.flaticon.com/512/8355/8355694.png
-// https://cdn-icons-png.flaticon.com/512/3901/3901586.png
-// https://cdn-icons-png.flaticon.com/512/8353/8353823.png
-// https://cdn-icons-png.flaticon.com/512/8351/8351887.png
