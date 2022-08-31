2a1164329afb80bce8b8747c2c00413f4c5f738f
Joseph Ogbu <57672199+fallyfox@users.noreply.github.com>
2022-08-31 14:35:53 +0100

    Create logos.js

---
+++ logos.js
@@ -0,0 +1,6 @@
+// https://cdn-icons-png.flaticon.com/512/2869/2869818.png
+// https://cdn-icons-png.flaticon.com/512/1048/1048611.png
+// https://cdn-icons-png.flaticon.com/512/8355/8355694.png
+// https://cdn-icons-png.flaticon.com/512/3901/3901586.png
+// https://cdn-icons-png.flaticon.com/512/8353/8353823.png
+// 
